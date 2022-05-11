6be9ef4d4047b871746e6af1327722a46b3dc7e6
anton.pashutkin <anton.pashutkin@gxbventures.com>
2022-05-11 14:23:56 +0300

    project skeleton

---
+++ src/test/java/ru/tester836/springboottesting/SpringBootTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tester836.springboottesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
